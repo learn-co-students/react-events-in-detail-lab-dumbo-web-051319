98b4cbf8cf78e0403b9b8550001ac1af54ff941c
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-18 00:06:47 -0400

    practiced using more events

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+  handleClick = (event) => {
+    // if you dont add event.persist() your event will be undefined due to event pooling
+    event.persist()
+    setTimeout(() => {
+      this.props.onDelayedClick(event)
+    } , this.props.delay)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>DELAY</button>
+      </div>
+    )
+  }
+
+}
